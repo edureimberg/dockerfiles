e27a5e728a022d1e1bfce5477190f939b99bc4ed
Eduardo <eduardo.reimberg@gmail.com>
2016-01-08 11:42:43 -0200

    Removendo spacewlak

--- spacewalk/web/config_spacewalk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Configura o spacewalk
-spacewalk-setup --disconnected --external-postgresql --answer-file=/root/answer_file.txt
-
-#Inicia o spacewalk
-spacewalk-service start
-
-#/usr/sbin/spacewalk-service start
